615ef841f874a3584a116a151b1e3f6775406d47
Diego <diego.garcia.santos22@estudiantes.uva.es>
2024-04-24 19:04:07 +0200

    adf

--- src/jerarquicos/AB.java
+++ src/jerarquicos/AB.java
@@ -94,4 +94,5 @@ public class AB <E>{
 
 
 
+
 }
--- src/jerarquicos/ABEnteros.java
+++ src/jerarquicos/ABEnteros.java
@@ -1,4 +1,14 @@
 package jerarquicos;
 
-public class ABEnteros {
+public class ABEnteros<Integer> extends AB{
+    public ABEnteros(NodoAB raiz) {
+        super(raiz);
+    }
+    public boolean comprobarSumas(NodoAB<Integer> nodo){
+        int sumaDer, sumaIzq;
+
+    }
+    public int sumarArboles(NodoAB<Integer> nodo){
+
+    }
 }
