0a8c57faf1ee28e305373c08d98ab986120ad8e5
Dimitar Vasilev Vasilev <dimitar.vasilev22@estudiantes.uva.es>
2024-04-24 19:25:17 +0200

    asmdas

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,13 @@ import jerarquicos.NodoAB;
 public class Main {
     public static void main(String[] args) {
         NodoAB<Integer> p, a, c, o, d;
-        p= new NodoAB<Integer>(null,null, 3);
-        a = new NodoAB<Integer>(p, null, 5);
-        c= new NodoAB<Integer>(null, a, 9);
-        d= new NodoAB<Integer>(null, null, -2);
-        o= new NodoAB<Integer>(d, c, 10);
+        p= new NodoAB<Integer>(null,null, 8);
+        a = new NodoAB<Integer>(null, p, 5);
+        d= new NodoAB<Integer>(null, null, 9);
+        p = new NodoAB<Integer>(null, null, 7);
+        c = new NodoAB<Integer>(d, p, 6);
+        o= new NodoAB<Integer>(c, a, 2);
         AB<Integer> arbol = new AB<Integer>(o);
-        System.out.println(arbol.listadoInOrdenConverso());
+        System.out.println(arbol.listadoInOrden());
     }
 }
--- src/jerarquicos/ABEnteros.java
+++ src/jerarquicos/ABEnteros.java
@@ -4,11 +4,27 @@ public class ABEnteros<Integer> extends AB{
     public ABEnteros(NodoAB raiz) {
         super(raiz);
     }
+
     public boolean comprobarSumas(NodoAB<Integer> nodo){
         int sumaDer, sumaIzq;
 
     }
     public int sumarArboles(NodoAB<Integer> nodo){
 
+    }
+
+    public boolean comprobarClavePequeña(){
+        if(raiz == null){
+            return true;
+        }
+        return comprobarClavePequeña(raiz);
+    }
+    private boolean comprobarClavePequeña(NodoAB<Integer> nodo){
+        boolean menor = true;
+        if(nodo != null){
+           //if (nodo.getIzq().getDato() < nodo.getDato()){
+
+           }
+        }
     }
 }
